ae6cb6b489127ae4616d91e5a43f103fa74e6a5c
Alexander Batashev <alex@batashev.me>
2026-03-05 20:14:56 +0300

    remove compose app

--- compose/composeApp/src/commonMain/kotlin/me/batashev/friday/Platform.kt
+++
@@ -1,7 +0,0 @@
-package me.batashev.friday
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- compose/composeApp/src/jvmMain/kotlin/me/batashev/friday/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package me.batashev.friday
-
-class JVMPlatform : Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
