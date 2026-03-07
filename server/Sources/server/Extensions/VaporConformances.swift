fa96933c0d5152e941d653299dbcf31ee353a487
Alexander Batashev <alex@batashev.me>
2026-03-07 11:13:59 +0300

    remove server (outdated)

--- server/Sources/server/Extensions/VaporConformances.swift
+++
@@ -1,9 +0,0 @@
-import Vapor
-import FridayAPI
-
-extension AuthResponse: @retroactive Content {}
-extension ConversationDTO: @retroactive Content {}
-extension ConversationTurnDTO: @retroactive Content {}
-extension CreateConversationRequest: @retroactive Content {}
-extension RegisterRequest: @retroactive Content {}
-extension LoginRequest: @retroactive Content {}
