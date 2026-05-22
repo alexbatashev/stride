3069bd65ee7dd61b55dc68caced9126c73adf181
Alexander Batashev <alex@batashev.me>
2026-05-22 08:42:08 +0300

    test(tinynet): add tls e2e coverage with test-mode verifier (#42)
    extend tinynet e2e tests with an https axum server and add a test-only insecure tls toggle used by integration tests.

--- libs/tinynet/src/stream.rs
+++ libs/tinynet/src/stream.rs
@@ -5,8 +5,11 @@ use std::task::{Context, Poll};
 
 use hyper::rt::{Read, ReadBufCursor, Write};
 use mio::net::TcpStream;
+use rustls::client::danger::{HandshakeSignatureValid, ServerCertVerified, ServerCertVerifier};
 use rustls::crypto::aws_lc_rs::default_provider;
+use rustls::pki_types::{CertificateDer, UnixTime};
 use rustls::{ClientConfig, ClientConnection, RootCertStore, pki_types::ServerName};
+use rustls::{DigitallySignedStruct, SignatureScheme};
 
 pub struct AsyncTlsStream {
     socket: TcpStream,
@@ -234,13 +237,65 @@ impl Write for AsyncTcpStream {
 fn make_config() -> Arc<ClientConfig> {
     let mut root_store = RootCertStore::empty();
     root_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
-    Arc::new(
-        ClientConfig::builder_with_provider(Arc::new(default_provider()))
-            .with_safe_default_protocol_versions()
-            .expect("default TLS versions are valid")
-            .with_root_certificates(root_store)
-            .with_no_client_auth(),
-    )
+    let mut config = ClientConfig::builder_with_provider(Arc::new(default_provider()))
+        .with_safe_default_protocol_versions()
+        .expect("default TLS versions are valid")
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+    if std::env::var_os("TINYNET_INSECURE_TLS").is_some() {
+        config
+            .dangerous()
+            .set_certificate_verifier(Arc::new(NoCertificateVerification));
+    }
+    Arc::new(config)
+}
+
+#[derive(Debug)]
+struct NoCertificateVerification;
+
+impl ServerCertVerifier for NoCertificateVerification {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &CertificateDer<'_>,
+        _intermediates: &[CertificateDer<'_>],
+        _server_name: &ServerName<'_>,
+        _ocsp_response: &[u8],
+        _now: UnixTime,
+    ) -> Result<ServerCertVerified, rustls::Error> {
+        Ok(ServerCertVerified::assertion())
+    }
+
+    fn verify_tls12_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer<'_>,
+        _dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn verify_tls13_signature(
+        &self,
+        _message: &[u8],
+        _cert: &CertificateDer<'_>,
+        _dss: &DigitallySignedStruct,
+    ) -> Result<HandshakeSignatureValid, rustls::Error> {
+        Ok(HandshakeSignatureValid::assertion())
+    }
+
+    fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
+        vec![
+            SignatureScheme::RSA_PKCS1_SHA256,
+            SignatureScheme::RSA_PKCS1_SHA384,
+            SignatureScheme::RSA_PKCS1_SHA512,
+            SignatureScheme::ECDSA_NISTP256_SHA256,
+            SignatureScheme::ECDSA_NISTP384_SHA384,
+            SignatureScheme::ED25519,
+            SignatureScheme::RSA_PSS_SHA256,
+            SignatureScheme::RSA_PSS_SHA384,
+            SignatureScheme::RSA_PSS_SHA512,
+        ]
+    }
 }
 
 fn is_pending_socket_error(err: &io::Error) -> bool {
